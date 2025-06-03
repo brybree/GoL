c5272b7e883c24757241f4435c7d7cc44d6744af
bonnty <43603875+brybree@users.noreply.github.com>
2025-06-04 00:15:14 +0200

    working infinite grid 'limit by memory only'

--- src/main.zig
+++ src/main.zig
@@ -22,71 +22,85 @@ pub fn main() !void {
     try bw.flush();
 
     // Lets start with a blinker
-    const grid = [3][3][]const u8{
-        .{ DEAD, DEAD, DEAD },
-        .{ ALIVE, ALIVE, ALIVE },
-        .{ DEAD, DEAD, DEAD },
-    };
+    const allocator = std.heap.page_allocator;
+
+    var grid = Grid.init(allocator);
+    defer grid.deinit();
+
+    try grid.set(0, 0, DEAD);
+    try grid.set(1, 0, DEAD);
+    try grid.set(2, 0, DEAD);
+    try grid.set(0, 1, ALIVE);
+    try grid.set(1, 1, ALIVE);
+    try grid.set(2, 1, ALIVE);
+    try grid.set(0, 2, DEAD);
+    try grid.set(1, 2, DEAD);
+    try grid.set(2, 2, DEAD);
+
     try writeGrid(@TypeOf(stdout), stdout, grid);
     try bw.flush();
 
-    const grid_cp = calculNextGeneration(grid);
+    const neighbors = [_][2]isize{
+        .{ -1, -1 }, .{ -1, 0 }, .{ -1, 1 },
+        .{ 0, -1 },  .{ 0, 1 },  .{ 1, -1 },
+        .{ 1, 0 },   .{ 1, 1 },
+    };
 
-    try writeGrid(@TypeOf(stdout), stdout, grid_cp);
-    try bw.flush(); // Don't forget to flush!
+    var gridcp = try grid.clone();
+    defer gridcp.deinit();
 
+    for (gridcp.rows.items, 0..) |row, row_index| {
+        for (row.items, 0..) |cell, cell_index| {
+            var alive_neighbors: usize = 0;
+
+            for (neighbors) |offset| {
+                const ni = @as(isize, @intCast(cell_index)) + offset[0];
+                const nj = @as(isize, @intCast(row_index)) + offset[1];
+
+                // check for boundaries
+                if (ni < 0 or nj < 0)
+                    continue;
+
+                const niu = @as(u8, @intCast(ni));
+                const nju = @as(u8, @intCast(nj));
+
+                // check for boundaries
+                if (nju >= gridcp.rows.items.len) continue;
+                const tmp_row = gridcp.rows.items[nju];
+                if (niu >= tmp_row.items.len) continue;
+
+                if (std.mem.eql(u8, try gridcp.get(niu, nju), ALIVE)) {
+                    alive_neighbors += 1;
+                }
+            }
+
+            if (std.mem.eql(u8, cell, DEAD)) {
+                if (alive_neighbors == 3) {
+                    try grid.set(cell_index, row_index, ALIVE);
+                }
+            }
+            if (std.mem.eql(u8, cell, ALIVE)) {
+                if (alive_neighbors < 2 or alive_neighbors > 3) {
+                    try grid.set(cell_index, row_index, DEAD);
+                }
+            }
+        }
+    }
+    try writeGrid(@TypeOf(stdout), stdout, grid);
+    try bw.flush();
 }
 
-fn writeGrid(comptime WriterType: type, w: WriterType, grid: [3][3][]const u8) !void {
-    for (grid) |row| {
-        for (row) |cell| {
+fn writeGrid(comptime WriterType: type, w: WriterType, grid: Grid) !void {
+    try w.print("\n", .{});
+    for (grid.rows.items) |row| {
+        for (row.items) |cell| {
             try w.print("{s} ", .{cell});
         }
         try w.print("\n", .{});
     }
 }
 
-fn calculNextGeneration(grid: [3][3][]const u8) [3][3][]const u8 {
-    // copy grid
-    var grid_copy = grid;
-    const neighbors = [_][2]isize{
-        .{ -1, -1 }, .{ -1, 0 }, .{ -1, 1 },
-        .{ 0, -1 },  .{ 0, 1 },  .{ 1, -1 },
-        .{ 1, 0 },   .{ 1, 1 },
-    };
-    // Calcul next generation
-    for (&grid_copy, 0..) |*row, i| {
-        for (row, 0..) |*cell, j| {
-            var alive_neighbors: usize = 0;
-
-            for (neighbors) |offset| {
-                const ni = @as(isize, @intCast(i)) + offset[0];
-                const nj = @as(isize, @intCast(j)) + offset[1];
-
-                if (ni < 0 or nj < 0 or ni >= grid.len or nj >= grid[0].len)
-                    continue;
-
-                const niu = @as(u8, @intCast(ni));
-                const nju = @as(u8, @intCast(nj));
-
-                if (std.mem.eql(u8, grid[niu][nju], ALIVE)) {
-                    alive_neighbors += 1;
-                }
-            }
-            if (std.mem.eql(u8, cell.*, DEAD)) {
-                if (alive_neighbors == 3) {
-                    cell.* = ALIVE;
-                }
-            }
-            if (std.mem.eql(u8, cell.*, ALIVE)) {
-                if (alive_neighbors < 2 or alive_neighbors > 3) {
-                    cell.* = DEAD;
-                }
-            }
-        }
-    }
-    return grid_copy;
-}
+//fn calculNextGeneration(grid: Grid) Grid {}
 
 test "blinker start horizontal" {
     const grid = [_][3][]const u8{
@@ -111,13 +125,13 @@ test "blinker start vertical" {}
 
 const Grid = struct {
     allocator: std.mem.Allocator,
-    rows: std.ArrayList(std.ArrayList(u8)),
+    rows: std.ArrayList(std.ArrayList([]const u8)),
 
     pub fn init(allocator: std.mem.Allocator) Grid {
         return Grid{
             .allocator = allocator,
-            .rows = std.ArrayList(std.ArrayList(u8)).init(allocator),
-       };
+            .rows = std.ArrayList(std.ArrayList([]const u8)).init(allocator),
+        };
     }
 
     pub fn deinit(self: *Grid) void {
@@ -127,23 +141,34 @@ const Grid = struct {
         self.rows.deinit();
     }
 
-    pub fn appendRow(self: *Grid) !void {
-        try self.rows.append(std.ArrayList(u8).init(self.allocator));
+    pub fn clone(self: *Grid) !Grid {
+        var new_grid = Grid.init(self.allocator);
+
+        for (self.rows.items) |row| {
+            var new_row = std.ArrayList([]const u8).init(self.allocator);
+            try new_row.appendSlice(row.items);
+            try new_grid.rows.append(new_row);
+        }
+        return new_grid;
     }
 
-    pub fn appendToRow(self: *Grid, rowIndex: usize, value: u8) !void {
+    pub fn appendRow(self: *Grid) !void {
+        try self.rows.append(std.ArrayList([]const u8).init(self.allocator));
+    }
+
+    pub fn appendToRow(self: *Grid, rowIndex: usize, value: []const u8) !void {
         if (rowIndex >= self.rows.items.len) return error.IndexOutOfBounds;
         try self.rows.items[rowIndex].append(value);
     }
 
-    pub fn get(self: *Grid, x: usize, y: usize) !u8 {
+    pub fn get(self: *Grid, x: usize, y: usize) ![]const u8 {
         if (y >= self.rows.items.len) return error.IndexOutOfBounds;
         const row = self.rows.items[y];
         if (x >= row.items.len) return error.IndexOutOfBounds;
         return row.items[x];
     }
 
-    pub fn set(self: *Grid, x: usize, y: usize, value: u8) !void {
+    pub fn set(self: *Grid, x: usize, y: usize, value: []const u8) !void {
         while (self.rows.items.len <= y) {
             try appendRow(self);
         }
@@ -151,38 +176,9 @@ const Grid = struct {
         var row = &self.rows.items[y];
 
         while (row.items.len <= x) {
-            try row.append(0);
+            try row.append(DEAD);
         }
 
         row.items[x] = value;
     }
 };
-
-pub fn mama() !void {
-    const allocator = std.heap.page_allocator;
-
-    var grid = Grid.init(allocator);
-    defer grid.deinit();
-
-    try grid.appendRow();
-    try grid.appendRow();
-    try grid.appendRow();
-
-    try grid.appendToRow(0, 1);
-    try grid.appendToRow(0, 2);
-    try grid.appendToRow(0, 3);
-    try grid.appendToRow(1, 4);
-    try grid.appendToRow(1, 5);
-    try grid.appendToRow(1, 6);
-    try grid.appendToRow(2, 7);
-    try grid.appendToRow(2, 8);
-
-    try grid.set(0, 2, 9);
-
-    for (grid.rows.items) |row| {
-        for (row.items) |cell| {
-            std.debug.print("{d}", .{cell});
-        }
-        std.debug.print("\n", .{});
-    }
-}
