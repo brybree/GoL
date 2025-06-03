b20f8ff38015783df7805330d364bec3ec81591a
bonnty <43603875+brybree@users.noreply.github.com>
2025-06-03 22:41:58 +0200

    another grid with arrayList for 'unlimited' dimensions

--- src/main.zig
+++ src/main.zig
@@ -108,3 +108,81 @@ test "blinker start horizontal" {
 }
 
 test "blinker start vertical" {}
+
+const Grid = struct {
+    allocator: std.mem.Allocator,
+    rows: std.ArrayList(std.ArrayList(u8)),
+
+    pub fn init(allocator: std.mem.Allocator) Grid {
+        return Grid{
+            .allocator = allocator,
+            .rows = std.ArrayList(std.ArrayList(u8)).init(allocator),
+       };
+    }
+
+    pub fn deinit(self: *Grid) void {
+        for (self.rows.items) |*row| {
+            row.deinit();
+        }
+        self.rows.deinit();
+    }
+
+    pub fn appendRow(self: *Grid) !void {
+        try self.rows.append(std.ArrayList(u8).init(self.allocator));
+    }
+
+    pub fn appendToRow(self: *Grid, rowIndex: usize, value: u8) !void {
+        if (rowIndex >= self.rows.items.len) return error.IndexOutOfBounds;
+        try self.rows.items[rowIndex].append(value);
+    }
+
+    pub fn get(self: *Grid, x: usize, y: usize) !u8 {
+        if (y >= self.rows.items.len) return error.IndexOutOfBounds;
+        const row = self.rows.items[y];
+        if (x >= row.items.len) return error.IndexOutOfBounds;
+        return row.items[x];
+    }
+
+    pub fn set(self: *Grid, x: usize, y: usize, value: u8) !void {
+        while (self.rows.items.len <= y) {
+            try appendRow(self);
+        }
+
+        var row = &self.rows.items[y];
+
+        while (row.items.len <= x) {
+            try row.append(0);
+        }
+
+        row.items[x] = value;
+    }
+};
+
+pub fn mama() !void {
+    const allocator = std.heap.page_allocator;
+
+    var grid = Grid.init(allocator);
+    defer grid.deinit();
+
+    try grid.appendRow();
+    try grid.appendRow();
+    try grid.appendRow();
+
+    try grid.appendToRow(0, 1);
+    try grid.appendToRow(0, 2);
+    try grid.appendToRow(0, 3);
+    try grid.appendToRow(1, 4);
+    try grid.appendToRow(1, 5);
+    try grid.appendToRow(1, 6);
+    try grid.appendToRow(2, 7);
+    try grid.appendToRow(2, 8);
+
+    try grid.set(0, 2, 9);
+
+    for (grid.rows.items) |row| {
+        for (row.items) |cell| {
+            std.debug.print("{d}", .{cell});
+        }
+        std.debug.print("\n", .{});
+    }
+}
